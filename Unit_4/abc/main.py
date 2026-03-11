1f08f8202722c9563e15c3a9eb0b295c25909dbd
Phillip Gachnang <pgachnang@gmail.com>
2026-03-11 09:40:18 +0000

    improvements based on feedback (Rainer)

--- Unit_4/abc/main.py
+++ Unit_4/abc/main.py
@@ -1,13 +1,12 @@
-from taxable_items import RegularProduct, ReducedProduct, TaxExemptProduct
+from taxable_items import RegularProduct, ReducedProduct, TaxFreeProduct
 from shopping_cart import ShoppingCart
 
 if __name__ == "__main__":
-    # Example usage
     cart = ShoppingCart()
 
     cart.add_item(RegularProduct("Headphones", 100.00))
     cart.add_item(ReducedProduct("Bread", 10.00))
-    cart.add_item(TaxExemptProduct("Postage Stamp", 5.00))
+    cart.add_item(TaxFreeProduct("Postage Stamp", 5.00))
 
     print(f"Net total:   {cart.total_net():.2f} CHF")
     print(f"VAT total:   {cart.total_vat():.2f} CHF")
